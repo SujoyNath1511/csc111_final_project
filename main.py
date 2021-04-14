a51af1fe10edfbb99615845a737c03ec516b939c
Sujoy Deb Nath <59268862+SujoyNath1511@users.noreply.github.com>
2021-04-14 18:03:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ if __name__ == '__main__':
         player1, player2 = players
         game = c_game.run_game_pygame(player1, player2)
         print('The winner:' + game[0])
+        pygame_interface.look_back_to_moves(game[1])
 
     ##################################################################
     # Print AI statistics. This part may take a while, so if you want to see
